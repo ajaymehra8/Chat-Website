f768d829447a3a6304bebfbad0b8ff4f402b5768
Ajay Mehra <ajaymehra4492@gmail.com>
2024-02-25 21:08:59 +0530

    solving issue of cors policy

--- Server/server.js
+++ Server/server.js
@@ -23,7 +23,7 @@ app.get("/",(req,res)=>{
   res.send("hello");
   });
 const corsOptions = {
-    origin: 'https://chat-website-client.vercel.app/', // Replace with your frontend's URL
+    origin: 'https://chat-website-client.vercel.app', // Replace with your frontend's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
     optionsSuccessStatus: 204,  // Some legacy browsers (IE11, various SmartTVs) choke on 204
