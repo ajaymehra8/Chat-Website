6c3ea132c61124047f3099d31588a1a4255d449b
Ajay Mehra <ajaymehra4492@gmail.com>
2024-02-25 20:46:28 +0530

    Updating cors policy

--- Server/server.js
+++ Server/server.js
@@ -21,7 +21,7 @@ app.use(express.json());
 //cors policy
 
 const corsOptions = {
-    origin: 'http://localhost:3000', // Replace with your frontend's URL
+    origin: 'https://chat-website-client.vercel.app/', // Replace with your frontend's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,  // Enable credentials (cookies, authorization headers, etc.)
     optionsSuccessStatus: 204,  // Some legacy browsers (IE11, various SmartTVs) choke on 204
